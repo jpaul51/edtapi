ee8753283487f489edb23075f752b52cb4f940cd
iem <iem@iutabgdin202-04.univ-lyon1.fr>
2017-03-06 10:22:17 +0100

    Project initialized

---
+++ src/main/java/com/iem/EdtApiApplication.java
@@ -0,0 +1,12 @@
+package com.iem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdtApiApplication.class, args);
+	}
+}
