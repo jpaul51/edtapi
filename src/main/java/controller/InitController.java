1a3746afe0a999a0a220e71e6d32bf3a1e2272a9
iem <iem@iutabgdin202-04.univ-lyon1.fr>
2017-03-27 08:57:30 +0200

    add calandar file

--- src/main/java/controller/InitController.java
+++ src/main/java/controller/InitController.java
@@ -1,5 +1,22 @@
 package controller;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Configuration
+@EnableJpaRepositories("dao")
+@EntityScan("model")
 public class InitController {
 
+	
+	
+	
+	
+	
 }
